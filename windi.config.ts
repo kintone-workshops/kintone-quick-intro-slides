bdd59396fd93a99f1c4da0b0a85391c1f9d7191d
Genji <genji-fujimori@cybozu.co.jp>
2023-11-15 11:55:12 +0900

    sync w/ hack-with-kintone slides

---
+++ windi.config.ts
@@ -0,0 +1,34 @@
+import { resolve } from 'path'
+import { mergeWindicssConfig, defineConfig } from 'vite-plugin-windicss'
+import BaseConfig from '@slidev/client/windi.config'
+
+// extend the base config
+export default mergeWindicssConfig(
+  BaseConfig,
+  defineConfig({
+    extract: {
+      include: [
+        resolve(__dirname, '**/*.{vue,ts}'),
+      ],
+    },
+    shortcuts: {
+      // custom the default background
+      'bg-main': 'bg-white text-[#181818] dark:(bg-[#121212] text-[#ddd])',
+    },
+    // theme: {
+    //   extend: {
+    //     colors: {
+    //       primary: {
+    //         DEFAULT: "#008080",
+    //         dark: "#038378",
+    //         darker: "#076E65",
+    //         darkest: "#065A53",
+    //         light: "#10B5A7",
+    //         lighter: "#0CCABA",
+    //         lightest: "#06E5D2",
+    //       },
+    //     },
+    //   },
+    // },
+  }),
+)
